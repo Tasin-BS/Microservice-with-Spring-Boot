83d9edc986a3335a407e639671a881dd9a9bf085
Tasin <Shahidullah.Tasin@brainstation-23.com>
2024-10-08 19:28:09 +0600

    customer dto , sql, repository impl

--- accounts/src/main/java/com/bank_management/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/bank_management/accounts/controller/AccountController.java
@@ -1,13 +1,25 @@
 package com.bank_management.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.bank_management.accounts.dto.CustomerDto;
+import com.bank_management.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
+
 @RestController
+@RequestMapping(path="/api", produces={MediaType.APPLICATION_JSON_VALUE})
 public class AccountController {
 
-    @GetMapping("sayHello")
-    public String sayHello(){
-        return "Hello world";
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 }
